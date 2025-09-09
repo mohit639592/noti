e5a9dcaf4d97155cb1f57dda2b2d307db8747a50
mohit639592 <mohitsharma639592@gmail.com>
2025-09-09 20:09:30 +0530

    First

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 const connect = require("./src/database/db");
-const port = 4000;
+const port =  process.env.PORT ||4000;
 
 app.listen(port,()=>{
     console.log("SERVER SUCCESSFULLY CONNECTED");
--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connect = ()=>{
-    mongoose.connect("mongodb://localhost:27017/")
+    mongoose.connect("mongodb+srv://Mohit:ZyaypO1HFkmNo4KR@cluster0.safgwzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(()=>{
         console.log("SERVER CONNECTED");
     })
